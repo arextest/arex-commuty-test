7d1e924b93bb9bfe93cffe78a162547092161844
daixq <daixq@trip.com>
2023-01-17 11:39:16 +0800

    feat: update /hello

--- src/main/java/com/arextest/agent/test/controller/HelloController.java
+++ src/main/java/com/arextest/agent/test/controller/HelloController.java
@@ -1,8 +1,5 @@
 package com.arextest.agent.test.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +23,6 @@ public class HelloController {
     }
     @GetMapping("/hello")
     public String hello(){
-        SecurityContext context = SecurityContextHolder.getContext();
-        Authentication authentication = context.getAuthentication();
-        return "{\"response\":\"Hello, " + authentication.getName() + "!\"}";
+        return "{\"response\":\"welcome\"}";
     }
 }
