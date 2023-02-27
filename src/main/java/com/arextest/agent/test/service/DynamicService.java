7a8c6060e1e8e952009cdd06d02b899cda33fb2b
daixq <daixq@trip.com>
2023-02-27 17:21:22 +0800

    feat: add tests for dynamic method with return type of CompletableFuture

--- src/main/java/com/arextest/agent/test/service/DynamicService.java
+++ src/main/java/com/arextest/agent/test/service/DynamicService.java
@@ -1,13 +1,30 @@
 package com.arextest.agent.test.service;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import com.arextest.agent.test.entity.Mealrecomrestaurant;
+import com.arextest.agent.test.mapper.MybatisPlusMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DynamicService {
+    @Autowired
+    MybatisPlusMapper plusMapper;
+
     public String readFile(String fileName) {
         File file = new File(fileName);
         throw new RuntimeException(fileName + "FileNotFoundException 2");
+    }
+
+    public CompletableFuture<List<Mealrecomrestaurant>> getRestaurants() {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    QueryWrapper<Mealrecomrestaurant> queryWrapper = new QueryWrapper<>();
+                    queryWrapper.lambda().eq(Mealrecomrestaurant::getRestaurantName, "FOOD");
+                    return plusMapper.selectList(queryWrapper);
+                });
     }
 }
