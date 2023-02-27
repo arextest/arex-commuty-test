59d825f6fcc1622e27e3c952badfaf7d95bb9a47
daixq <daixq@trip.com>
2023-02-27 17:20:43 +0800

    feat: add tests for dynamic method with return type of CompletableFuture

--- src/main/java/com/arextest/agent/test/controller/DynamicTestController.java
+++ src/main/java/com/arextest/agent/test/controller/DynamicTestController.java
@@ -1,19 +1,27 @@
 package com.arextest.agent.test.controller;
 
+import com.arextest.agent.test.entity.Mealrecomrestaurant;
 import com.arextest.agent.test.entity.Request;
+import com.arextest.agent.test.mapper.MybatisPlusMapper;
 import com.arextest.agent.test.service.DynamicService;
-import java.io.IOException;
-
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import jodd.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.util.annotation.Nullable;
+
+
 @Controller
+@Slf4j
 @RequestMapping(value = "/dynamicTest")
 public class DynamicTestController {
+    @Autowired
+    MybatisPlusMapper plusMapper;
     @Autowired
     DynamicService dynamicService;
 
@@ -26,5 +34,23 @@ public class DynamicTestController {
         } catch (Exception e) {
             return e.getMessage();
         }
+    }
+
+    @RequestMapping (value = "/testReturnCompletableFuture")
+    @ResponseBody
+    public String testReturnCompletableFuture() {
+        CompletableFuture<List<Mealrecomrestaurant>> future = dynamicService.getRestaurants();
+        future.whenComplete(((restaurants, throwable) -> {
+            if (throwable != null) {
+                log.error("testReturnCompletableFuture", throwable);
+            }
+        }));
+        String restaurantNumber = "query failed";
+        try {
+            restaurantNumber = String.valueOf(future.get().size());
+        } catch (Exception ex) {
+            log.error("future.get", ex);
+        }
+        return "{\"Restaurant number\":\""+ restaurantNumber +"\"}";
     }
 }
